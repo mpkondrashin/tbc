bee3c31724bdc7ffcdba2ab2c3e59c44a46ba3ac
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-05 14:40:42 +0300

    commit

--- cmd/test/r.py
+++ cmd/test/r.py
@@ -5,7 +5,12 @@ headers = {
     'Content-Type': 'application/xml',
     'X-SMS-API-KEY': sys.argv[2],
 }
+
+url = f"https://{sys.argv[1]}/ipsProfileMgmt/getFilters"
 xml='<getFilters><profile name="test"></profile><filter><number>51</number></filter></getFilters>'
-r = requests.post(f"https://{sys.argv[1]}/ipsProfileMgmt/getFilters", data=xml)
+print(url)
+print(headers)
+print(xml)
+r = requests.post(url, data=xml)
 print(r.text)
 
