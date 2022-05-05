717459c23d108dced28061490b2fbf4e0c6f4e94
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-05 14:41:22 +0300

    commit

--- cmd/test/r.py
+++ cmd/test/r.py
@@ -3,10 +3,10 @@ import sys
 
 headers = {
     'Content-Type': 'application/xml',
-    'X-SMS-API-KEY': sys.argv[2],
+    'X-SMS-API-KEY': sys.argv[1],
 }
 
-url = f"https://{sys.argv[1]}/ipsProfileMgmt/getFilters"
+url = f"https://{sys.argv[2]}/ipsProfileMgmt/getFilters"
 xml='<getFilters><profile name="test"></profile><filter><number>51</number></filter></getFilters>'
 print(url)
 print(headers)
