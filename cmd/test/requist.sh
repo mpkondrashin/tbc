d53e54fe2fddab297bfa60e7a1c5e6785291ffc6
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-05 13:00:09 +0300

    commit

--- cmd/test/requist.sh
+++ cmd/test/requist.sh
@@ -2,4 +2,4 @@
 
 #curl -i -k --header "X-SMS-API-KEY:${1}" -F "requestFile=@get_filter.xml" "https://${2}/ipsProfileMgmt/getFilters"
 
-curl -v -v -v  -X POST -k --header "X-SMS-API-KEY: ${1}" --form name=@getFilters.xml https://${2}/ipsProfileMgmt/getFilters
+curl --trace-ascii tr.txt -v  -X POST -k --header "X-SMS-API-KEY: ${1}" --form name=@getFilters.xml https://${2}/ipsProfileMgmt/getFilters
