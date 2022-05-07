76a26cc2cd64fe9ba11212410927b1124b6e51e9
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-07 13:36:15 +0300

    add get filteres response struct

--- pkg/sms/ipsProfileMgmt_getFilters.go
+++ pkg/sms/ipsProfileMgmt_getFilters.go
@@ -19,3 +19,34 @@ type Filter struct {
 	SignatureID string `xml:"signature-id,omitempty"`
 	PolicyID    string `xml:"policy-id,omitempty"`
 }
+
+// Response
+
+type Filters struct {
+	XMLName xml.Name `xml:"filters"`
+	Text    string   `xml:",chardata"`
+	Profile struct {
+		Text    string `xml:",chardata"`
+		Name    string `xml:"name,attr"`
+		ID      string `xml:"id,attr"`
+		Version string `xml:"version,attr"`
+	} `xml:"profile"`
+	Filter struct {
+		Text      string `xml:",chardata"`
+		Name      string `xml:"name"`
+		PolicyID  string `xml:"policy-id"`
+		Version   string `xml:"version"`
+		Locked    string `xml:"locked"`
+		UseParent string `xml:"useParent"`
+		Severity  string `xml:"severity"`
+		Enabled   string `xml:"enabled"`
+		Actionset struct {
+			Text  string `xml:",chardata"`
+			Refid string `xml:"refid,attr"`
+			Name  string `xml:"name,attr"`
+		} `xml:"actionset"`
+		Control string `xml:"control"`
+		Afc     string `xml:"afc"`
+	} `xml:"filter"`
+} 
+
