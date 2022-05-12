6f9d4768aa2cdbbd75943a264d9f07eac3f4fa99
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-05-12 21:29:45 +0300

    commit

--- cmd/tbc/main.go
+++ cmd/tbc/main.go
@@ -55,7 +55,7 @@ func main() {
 	auth := sms.NewAPIKeyAuthorization(apiKey)
 	smsClient := sms.New(url, auth).SetInsecureSkipVerify(insecureSkipVerify)
 	//smsClient.DataDictionaryAll()
-	fmt.Println(smsClient.GetActionSet())
+	//fmt.Println(smsClient.GetActionSet())
 	//a, e := smsClient.DataDictionary("CATEGORY")
 	//fmt.Println(e, "CATEGORY ", a)
 	//return
